bc1b312ab84e94f0262d10e97781ef092f417dcd
DanielLChapman <danielc@my.ccsu.edu>
2021-12-02 15:46:43 -0500

    Shooter pathing done

---
+++ javascript/helperFunctions.js
@@ -0,0 +1,9 @@
+
+
+function calculateWidth(hyp, a) {
+    return hyp * Math.cos(a * Math.PI/180);
+}
+
+function calculateHeight(hyp,a) {
+    return hyp * Math.sin(a * Math.PI / 180);
+}
--- javascript/main.js
+++ javascript/main.js
@@ -1,23 +1,23 @@
-const canvas=document.querySelector("#canvas");
-const message = document.querySelector('#message');
-const ctx=canvas.getContext("2d");
+const canvas = document.querySelector("#canvas");
+const message = document.querySelector("#message");
+const ctx = canvas.getContext("2d");
 //console.log(ctx);
-canvas.width=window.innerWidth;
-canvas.height=window.innerHeight;
-message.style.display = 'none'; 
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+message.style.display = "none";
 let resize = false;
 
-window.addEventListener("resize",(e)=>{
+window.addEventListener("resize", (e) => {
     resize = true;
-  canvas.width=window.innerWidth;
-  canvas.height=window.innerHeight;
-  if (window.innerWidth < 800 || window.innerHeight < 600) {
-    canvas.style.display = 'none';
-    message.style.display = 'block';
-  } else {
-    canvas.style.display = 'block';
-    message.style.display = 'none'; 
-  }
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    if (window.innerWidth < 800 || window.innerHeight < 600) {
+        canvas.style.display = "none";
+        message.style.display = "block";
+    } else {
+        canvas.style.display = "block";
+        message.style.display = "none";
+    }
 });
 
 class Player {
@@ -26,8 +26,10 @@ class Player {
         this.height = 300;
         this.opacity = 1;
         this.backgroundColor = "#363636";
-        this.shooterColor = "black"
+        this.shooterColor = "black";
         this.x = 0;
+        this.xShooterSymbol = 1;
+        this.yShooterSymbol = 1;
         this.y = 0;
         this.exit = false;
         this.degrees = 90;
@@ -35,53 +37,101 @@ class Player {
         this.maxDegreesChange = 90;
         this.startPosition = 0;
         this.numShots = 1;
-    };
+    }
 
     drawShooter() {
-        //draw a rect with an angle 
+        let xCoord =
+            this.x === 0
+                ? PlayBoard.x + 10
+                : PlayBoard.x + PlayBoard.width - 10;
 
+        let yCoord =
+            this.y === 0
+                ? PlayBoard.y + 10
+                : PlayBoard.y + PlayBoard.height - 10;
+        //draw a rect with an angle
+        ctx.beginPath();
+        ctx.fillStyle = this.shooterColor;
+        ctx.strokeStyle = "#000";
+        ctx.lineWidth = 10;
+        ctx.moveTo(xCoord, yCoord);
+        ctx.lineTo(
+            xCoord + calculateWidth(60, this.degrees),
+            yCoord + calculateHeight(60, this.degrees)
+        );
+        ctx.fill();
+        this.degrees += this.progression;
+        if (
+            this.degrees === this.maxDegreesChange ||
+            this.degrees === this.startPosition
+        ) {
+            this.progression *= -1;
+        }
+
+        ctx.stroke();
+        /* 
+
+        this.check = 1;
+        this.count = 1;
+        this.degrees = 90;
+        this.minDegrees = 0;
+        
+        ctx.rotate(this.check * Math.PI / 180);
+        this.count += this.check;
+        if (this.count === this.degrees || this.count === this.minDegrees) {
+            this.check *= -1;
+        }
+        
+        
+        */
     }
 
     setPlayer = (count) => {
-        
         if (count === 1) {
             this.x = 0;
             this.y = 0;
-            this.startPosition = 0;
             this.shooterColor = "green";
-            this.maxDegreesChange = -90;
+            this.startPosition = 89;
+            this.degrees = 89;
+           
+            this.maxDegreesChange = 1;
             this.progression = -1;
         }
         if (count === 2) {
             this.x = canvas.width - this.width;
             this.y = 0;
-            this.startPosition = 180;
             this.shooterColor = "red";
-            this.maxDegreesChange = 270;
+            this.startPosition = -181;
+            this.degrees = -181;
+           
+            this.maxDegreesChange = -271;
+            this.progression = -1;
         }
         if (count === 3) {
-            this.x = canvas.height - this.height;
-            this.y = 0;
-            this.startPosition = 0;
+            this.x = 0;
+            this.y = canvas.height - this.height;
             this.shooterColor = "yellow";
-            this.maxDegreesChange = 90;
+            this.startPosition = -1;
+            this.degrees = -1;
+           
+            this.maxDegreesChange = -89;
+            this.progression = -1;
         }
         if (count === 4) {
             this.x = canvas.width - this.width;
             this.y = canvas.height - this.height;
-            this.startPosition = 180;
             this.shooterColor = "purple";
-            this.maxDegreesChange = 90;
+            this.startPosition = -91;
+            this.degrees = -91;
+           
+            this.maxDegreesChange = -179;
             this.progression = -1;
-        
         }
-    }
+    };
 }
 
-
-
 function rnd(min, max) {
-  return Math.floor(Math.random() * max) + min;
+    return Math.floor(Math.random() * max) + min;
 }
 
 class Block {
@@ -97,9 +147,13 @@ class Block {
         //if the colors are the same, ignore colision
         if (color === this.color) {
             return false;
-        }
-        else {
-            if(ballX >this.x && ballX < this.x+this.width && this.ballY > this.y && this.ballY < this.y+this.height) {
+        } else {
+            if (
+                ballX > this.x &&
+                ballX < this.x + this.width &&
+                this.ballY > this.y &&
+                this.ballY < this.y + this.height
+            ) {
                 this.color = color;
                 return true;
             }
@@ -109,30 +163,30 @@ class Block {
 
     draw() {
         ctx.beginPath();
-        ctx.fillStyle=this.color;
+        ctx.fillStyle = this.color;
         ctx.strokeStyle = "#000";
         ctx.lineWidth = 2;
-        ctx.rect(this.x,this.y,20,20);
+        ctx.rect(this.x, this.y, 20, 20);
         ctx.fill();
+
         ctx.stroke();
     }
-};
-
+}
 
 class Board {
     constructor() {
         this.x = 0 + Player1.width + 5;
         this.y = 5;
-        this.width = canvas.width - Player1.width*2 - 10;
+        this.width = canvas.width - Player1.width * 2 - 10;
         this.height = canvas.height - 15;
         this.backgroundColor = "#363636";
         this.color = "#000";
         this.gameRunning = false;
         this.needToResetBoard = true;
-    };
+    }
 
     updateSize() {
-        this.width = canvas.width - Player1.width*2 - 10;
+        this.width = canvas.width - Player1.width * 2 - 10;
         this.height = canvas.height - 15;
     }
 
@@ -148,20 +202,19 @@ class Board {
             this.needToResetBoard = true;
             resize = false;
         }
-        
+
         ctx.beginPath();
-        ctx.fillStyle=this.backgroundColor;
-        ctx.strokeStyle=this.color;
+        ctx.fillStyle = this.backgroundColor;
+        ctx.strokeStyle = this.color;
         ctx.lineWidth = 2;
-        ctx.fillRect(this.x,this.y,this.width,this.height);
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+
         ctx.stroke();
     }
 }
 
-
 const Player1 = new Player();
 Player1.setPlayer(1);
-
 
 const Player2 = new Player();
 Player2.setPlayer(2);
@@ -190,27 +243,27 @@ function calculateNumOfBlocks() {
     let tempBlock;
     for (let x = 0; x < totalAmount; x++) {
         //color switching
-        if (blockX < ((width / 2) * 20 + PlayBoard.x)) {//((PlayBoard.width / 2)+ PlayBoard.x - 19)) {
-            if (blockY <= ((PlayBoard.height + PlayBoard.y) / 2)) {
+        if (blockX < (width / 2) * 20 + PlayBoard.x) {
+            //((PlayBoard.width / 2)+ PlayBoard.x - 19)) {
+            if (blockY <= (PlayBoard.height + PlayBoard.y) / 2) {
                 //player 1 green
-                tempBlock = new Block(blockX, blockY, Player1.shooterColor)
+                tempBlock = new Block(blockX, blockY, Player1.shooterColor);
             } else {
                 //player 3 yellow
-                tempBlock = new Block(blockX, blockY, Player3.shooterColor)
+                tempBlock = new Block(blockX, blockY, Player3.shooterColor);
             }
-            
         } else {
-            if (blockY <= ((PlayBoard.height + PlayBoard.y) / 2)) {
+            if (blockY <= (PlayBoard.height + PlayBoard.y) / 2) {
                 //player 2 red
-                tempBlock = new Block(blockX, blockY, Player2.shooterColor)
+                tempBlock = new Block(blockX, blockY, Player2.shooterColor);
             } else {
                 //player 4 purple
-                tempBlock = new Block(blockX, blockY, Player4.shooterColor)
+                tempBlock = new Block(blockX, blockY, Player4.shooterColor);
             }
         }
         blockX = blockX + 20;
 
-        if (blockX + 20 >= PlayBoard.width  + PlayBoard.x + 19) {
+        if (blockX + 20 >= PlayBoard.width + PlayBoard.x + 19) {
             blockX = PlayBoard.x;
             blockY += 20;
         }
@@ -219,17 +272,21 @@ function calculateNumOfBlocks() {
     }
 }
 
-
-function animation(){
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  PlayBoard.draw();
-  if (PlayBoard.needToResetBoard) {
-      calculateNumOfBlocks();
-      PlayBoard.needToResetBoard = false;
-  }
-  blocks.forEach((x) => {
-      x.draw();
-  })
-  window.requestAnimationFrame(animation);
+function animation() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    PlayBoard.draw();
+    if (PlayBoard.needToResetBoard) {
+        calculateNumOfBlocks();
+        PlayBoard.needToResetBoard = false;
+    }
+    blocks.forEach((x) => {
+        x.draw();
+    });
+    players.forEach((x) => {
+        x.drawShooter();
+    });
+    setTimeout(() => {
+        window.requestAnimationFrame(animation);
+    }, 50);
 }
 animation();
