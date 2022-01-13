a1cba1372a46cdea65fbde0e014fdc518308982f
DanielLChapman <danielc@my.ccsu.edu>
2022-01-13 15:44:08 -0500

    Plinko cleanup and extraneous information deleted

--- javascript/plinko/setup.js
+++ javascript/plinko/setup.js
@@ -16,27 +16,41 @@ var Engine = Matter.Engine,
     Events = Matter.Events;
 var engine;
 var world;
+var mConstraint;
+
+//p = particles, plinkos = plinkos and bounds are rectangular plinkos essentially
 var p = [];
 var plinkos = [];
-var cols = 10;
-var rows = 10;
+
 var bounds = [];
-var mConstraint;
+
+//So as not to cause any confusion with the other canvas, canvas2 bottomB. 
 var bottomB;
 var canvas2;
-var btn;
+
+//Particle size
+let plinkoParticleSize = 6;
+
+//Base values that can be modified
+//cols and rows are how many columns and rows to fill
+let cols = 10;
+let rows = 10;
+//Minimum plinko size and max, these are what the particles bounce off of
 let plinkoMinSize = 1;
 let plinkoMaxSize = 5;
-let plinkoParticleSize = 6;
 let gravity = 2;
+
+//Mouse click switch for whether to drop your own particles or not
 let ownDrop = false;
 
+//Dropping a new particle based on the given inputs and pushes it to the array
 function newParticle(_x, _y, i, color = "orange") {
     let pA = new Particle(_x, _y, plinkoParticleSize, i, color);
     // console.log(pA.body)
     p.push(pA);
 }
 
+//Switch function to allow a player to drop their own plinkos and play with it seperately from the shooter game
 function ownDropChange() {
     ownDrop = !ownDrop;
     if (ownDrop) {
@@ -46,68 +60,36 @@ function ownDropChange() {
     }
 }
 
+//Initial setup
 function setup() {
+    //establish color mode
     colorMode(HSB);
+    //create a canvas
     canvas2 = createCanvas(240, windowHeight - 300);
+    //place in parent container
     canvas2.parent(document.querySelector(".plinko-container"));
-    rows = Math.ceil(height / 50) + 2;
+    //create physics engines
     engine = Engine.create();
     world = engine.world;
 
-    btn = select("#btn");
-
-    function collision(event) {
-        /*       if (event.pairs.length >= 2) {
-            var pairs = event.pairs;
-            for(let i = 0; i < pairs.length;i++){
-                var bodyA = pairs[i].bodyA;
-                var bodyB = pairs[i].bodyB;
-            }
-
-        } */
-        /*
-    var pairs = event.pairs;
-    for(let i = 0; i < pairs.length;i++){
-      var bodyA = pairs[i].bodyA;
-      var bodyB = pairs[i].bodyB;
-      var idA = bodyA.id;
-      var idB = bodyB.id;
-      // console.log(idA);
-      for(let i =0;i<plinkos.length;i++){
-        if(plinkos[i].body.id == idA || plinkos[i].body.id ==idB){
-          plinkos[i].shake();
-          // console.log('change')
-        }
-      }
-
-    }*/
-    }
-
-    Events.on(engine, "collisionStart", collision);
-
+    //run initial setup
     windowSetup();
 
-    var canvasmouse = Mouse.create(canvas2.elt);
-    canvasmouse.pixelRatio = pixelDensity();
-    var options = {
-        mouse: canvasmouse,
-    };
-
-    mConstraint = MouseConstraint.create(engine, options);
-    World.add(world, mConstraint);
 }
-
 
 
 function mousePressed() {
   
+    //grab the mouse position
     let mouseXClick = mouseX;
     let mouseYClick = mouseY;
+    //if we are dropping out own
     if (ownDrop) {
+        //if we are within the bounds of this canvas
         if (!(mouseXClick > width || mouseXClick < 0)) {
             if (!(mouseYClick > height - 80 || mouseYClick < 0)) {
+                //create our own particle with player -1 to not influence the game.
                 let color = `rgba(${Math.floor(random(0, 256))}, ${Math.floor(random(0, 256))},${Math.floor(random(0, 256))},1)`;
-                let playerValueColor = Math.floor(random(1, 5));
                 newParticle(
                     mouseX,
                     mouseY,
@@ -117,14 +99,7 @@ function mousePressed() {
             }
         }
     } else {
-        /*     //will eventually be players left;
-    for (let i = 1; i <= 4; i++) {
-        newParticle(random(0, width), 0, i);
-    } */
-
-        //let plinko = new Plinko(mouseX, mouseY, random(10,16));
-        //plinkos.push(plinko);
-
+        //If we are within the bounds
         if (!(mouseXClick > width || mouseXClick < 0)) {
             if (!(mouseYClick > height - 80 || mouseYClick < 0)) {
                 //check if we are inside a plinko
@@ -141,7 +116,7 @@ function mousePressed() {
                         remove.push(i);
                     }
                 });
-
+                //remove it from the array if there is one.
                 if (remove.length > 0) {
                     remove.forEach((x) => {
                         plinkos.splice(x, 1);
@@ -161,6 +136,8 @@ function mousePressed() {
     }
 }
 
+//Removes the boundaries from the world and resets the array
+//Otherwise the invisible boundaries would still exist.
 function clearBoundrys() {
     bounds.forEach((x) => {
         World.remove(world, x.body);
@@ -169,6 +146,7 @@ function clearBoundrys() {
     bounds = [];
 }
 
+//Remove the plinkos from the world and array, otherwise they would still exist just be invisible.
 function clearPlinkos() {
     plinkos.forEach((x) => {
         World.remove(world, x.body);
@@ -176,10 +154,14 @@ function clearPlinkos() {
     plinkos = [];
 }
 
+//Resetting the plinkos back to original
 function resetPlinkos() {
+    //clear the plinkos
     clearPlinkos();
+    //establish how many plinkos we need per row and column
     let spacewidth = width / cols;
     let spaceheight = height / rows;
+    //loop over all options and then create plinkos with enough space in between rows and columns so that they all fit
     for (let i = 0; i < rows; i++) {
         for (let j = 0; j < cols + 1; j++) {
             let x = j * spacewidth;
@@ -198,25 +180,35 @@ function resetPlinkos() {
     }
 }
 
+//draw function
 function draw() {
+    //websites background color
     background("#363636");
     // console.log(frameRate())
+    //if we aren't plinko playing, clear
     if (!plinkoPlay) {
         Engine.clear(engine);
     } else {
+        //Otherwise run it at 60 fps (if possible)
         Engine.update(engine, 1000 / 60);
     }
 
+    //For all particles
     for (let i = 0; i < p.length; i++) {
+        //draw it
         p[i].show();
+        //check if its off screen
         if (p[i].isOffScreen()) {
             World.remove(world, p[i].body);
             p.splice(i, 1);
             i--;
         }
+        //Otherwise grab the position
         let x = p[i]?.body?.position?.x;
         let y = p[i]?.body?.position?.y;
 
+        //If we are at the end of the board, update the player's shot 
+        //Each particle contains players values
         if (y > windowHeight - 308) {
             if (x <= width / 3) {
                 players[p[i].player - 1]?.updateShots("addition");
@@ -231,38 +223,37 @@ function draw() {
         }
     }
 
+    //Show all plinkos
     for (let i = 0; i < plinkos.length; i++) {
         plinkos[i].show();
     }
 
+    //Show all bounds (some are out of bounds of the canvas)
     for (let i = 0; i < bounds.length; i++) {
         bounds[i].show();
     }
-
-    if (mConstraint.body) {
-        let pos = mConstraint.body.position;
-        let offset = mConstraint.constraint.pointB;
-        let m = mConstraint.mouse.position;
-        // console.log(mConstraint.body)
-        stroke(180, 255, 255);
-        line(pos.x + offset.x, pos.y + offset.y, m.x, m.y);
-    }
 }
 
+//initial set up
 function windowSetup() {
+    //establish a gravity for the world
     world.gravity.y = gravity;
+    //reset the plinkos
     resetPlinkos();
+    //clear boundaries
     clearBoundrys();
+    //remake the bottom boundary
     bottomB = new Boundry(width / 2, height + 50, width, 100);
     bounds.push(bottomB);
 
-    // walls
+    // remake the walls out of bounds
     wallLeft = new Boundry(-5, 0, 2, height * 2);
     bounds.push(wallLeft);
 
     wallRight = new Boundry(width + 5, 0, 2, height * 2);
     bounds.push(wallRight);
 
+    //position the bottom buckets in the right position (so 4 walls to make 3 buckets).
     for (let i = 0; i < 4; i++) {
         let w = 4;
         let h = 80;
@@ -279,12 +270,14 @@ function windowSetup() {
     }
 }
 
+//Resize function to update the height mostly. 
 function windowResized() {
     resizeCanvas(240, windowHeight - 300);
-    rows = Math.ceil(height / 50) + 2;
+    //rerun setup
     windowSetup();
 }
 
+//resize and call this function only after 100ms since the last one.
 let limit;
 window.addEventListener("resize", () => {
     clearTimeout(limit);
